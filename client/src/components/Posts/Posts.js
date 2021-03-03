21af031c6e4b8d6be8e86bcd4de8538995fc49e7
LcNguyn <vinzeroy28@gmail.com>
2021-03-03 14:21:34 +0700

    Added eslintrc.js prettierrc

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-import Post from './Post/Post'
+import Post from './Post/Post';
 import useStyles from './styles';
 
-
 const Posts = () => {
-    const posts = useSelector( (state) => state.posts );
-    // eslint-disable-next-line
-    const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  // eslint-disable-next-line
+  const classes = useStyles();
 
-    console.log(posts);
-    return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
-    );
-}
+  // eslint-disable-next-line no-console
+  console.log(posts);
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        // eslint-disable-next-line no-underscore-dangle
+        <Grid key={post._id} item sx={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default Posts;
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,11 @@
 // eslint-disable-next-line
 export default (posts = [], action) => {
-    switch (action.type) {
-        case 'FETCH_ALL':
-            return action.payload;
-        case 'CREATE':
-            return [...posts, action.payload];
-        default:
-            return posts;
-    }
-}
-
+  switch (action.type) {
+    case 'FETCH_ALL':
+      return action.payload;
+    case 'CREATE':
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
